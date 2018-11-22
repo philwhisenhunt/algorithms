def387f3072ddaa6a678d5cb95c986bc5a77468f
Philip Whisenhunt <philip.whisenhunt@gmail.com>
2018-11-21 22:49:29 -0500

    checking in new class with protected variables added

--- index.php
+++ index.php
@@ -8,8 +8,13 @@ require 'functions.php';
 //Todo, Comment, User
 class Task {
 
+    protected $description;
+    protected $completed = false;
+    
     public function __construct($description)
     {
+        
+
         //Automatically triggered on instantiation
         $this-> description = $description;
     }
