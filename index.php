c2034c7c3dd0e9eba21ae70de1733c9e11234e40
Philip Whisenhunt <philip.whisenhunt@gmail.com>
2018-11-26 13:33:57 -0500

    Separated into task.php file

--- index.php
+++ index.php
@@ -1,21 +1,22 @@
 <?php
 
+require 'Task.php';
+
+
 try{
     $pdo = new PDO('mysql:host=127.0.0.1; dbname=mytodo', 'root', '');
 
 }
 
 catch(PDOException $e){
-    die('Could not connect. ');
+    die($e->getMessage);
 }
 
 $statement = $pdo->prepare('select * from todos');
 
 $statement->execute();
 
-$results = $statement->fetchAll(PDO::FETCH_OBJ);
+// $tasks = $statement->fetchAll(PDO::FETCH_OBJ);
 
-var_dump($results[0]->description);
-
-
+$tasks = $statement->fetchAll(PDO::FETCH_CLASS, 'Task');
 require 'index.view.php';
