0c49652ad90625ab0d18ee4939bcd4ce03f286f3
Philip Whisenhunt <philip.whisenhunt@gmail.com>
2018-11-26 14:15:49 -0500

    Cleaned up code a bit more

--- functions.php
+++ functions.php
@@ -5,11 +5,24 @@ function dd($data){
     echo '</pre>';
 }
 
-function ageCheck($age){
-    if($age >= 18 ){
-        return true;
+function connectToDb()
+{
+    try{
+        return new PDO('mysql:host=127.0.0.1;dbname=mytodo', 'root', '');
+    
     }
-    else {
-        return false;
+    
+    catch (PDOException $e){
+        die($e->getMessage());
     }
 }
+
+function fetchAllTasks($pdo)
+{
+    $statement = $pdo->prepare('select * from todos');
+
+    $statement->execute();
+    
+    return $statement->fetchAll(PDO::FETCH_CLASS, 'Task');
+}
+
--- index.php
+++ index.php
@@ -1,26 +1,13 @@
 <?php
 
+
+
+require 'functions.php';
 require 'Task.php';
 
 
-try{
-    $pdo = new PDO('mysql:host=127.0.0.1; dbname=mytodo', 'root', '');
+$pdo = connectToDb();
 
-}
-
-catch(PDOException $e){
-    die($e->getMessage);
-}
-
-$statement = $pdo->prepare('select * from todos');
-
-$statement->execute();
-
-// $tasks = $statement->fetchAll(PDO::FETCH_OBJ);
-
-$tasks = $statement->fetchAll(PDO::FETCH_CLASS, 'Task');
-
-
-var_dump($tasks[0]->foobar());
+$tasks = fetchAllTasks($pdo);
 
 require 'index.view.php';
