ffacc130b364fb6eee1f30feb780f46a12ef5c0c
Philip Whisenhunt <philip.whisenhunt@gmail.com>
2018-11-28 15:29:35 -0500

    rearranged router, added request

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
 
 $database = require 'core/bootstrap.php';
 
-$uri = trim($_SERVER['REQUEST_URI'], '/');
 
-$router = Router::load('routes.php');
+require Router::load('routes.php')
+    ->direct(Request::uri());
+    
 
-$router->direct($uri);
