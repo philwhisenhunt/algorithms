690585e0e1fa7b41296f585c671e88e73bbb0a0e
Philip Whisenhunt <philip.whisenhunt@gmail.com>
2018-11-28 14:38:16 -0500

    Removed old router loader

--- index.php
+++ index.php
@@ -3,12 +3,7 @@
 
 $database = require 'core/bootstrap.php';
 
-
-$router = new Router;
-
-require 'routes.php';
-
 $uri = trim($_SERVER['REQUEST_URI'], '/');
 
-
-require $router->direct($uri);
+require Router::load('routes.php')
+    ->direct($uri);
