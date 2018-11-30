ac83ce14fff3e72d77bb7cd3401b2a8025c71122
Philip Whisenhunt <philip.whisenhunt@gmail.com>
2018-11-30 15:40:14 -0500

    new bootstrap settings

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -1,16 +1,10 @@
 <?php
 
-$app = [];
+App::bind('config', require 'config.php');
 
-$app['config'] = require 'config.php';
-
-
-require 'core/Router.php';
-require 'core/Request.php';
-require 'core/database/Connection.php';
-require 'core/database/QueryBuilder.php';
+$config = App::get('config');
 
 
-$app['database'] = new QueryBuilder(
-    Connection::make($app['config']['database'])
-);
+App::bind('database', new QueryBuilder(
+    Connection::make(App::get('config')['database'])
+));
