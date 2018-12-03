c8d8cd51024729bde4f4d5eaf1057170cea11ed3
Philip Whisenhunt <philip.whisenhunt@gmail.com>
2018-12-03 13:58:55 -0500

    Learned splat, added to router

--- core/Router.php
+++ core/Router.php
@@ -29,12 +29,27 @@ class Router
 
     public function direct($uri, $requestType)
     {
-        die(var_dump($uri, $requestType));
 
         if(array_key_exists($uri, $this->routes[$requestType])){
-            return $this->routes[$requestType][$uri];
+            
+            //PagesController@home
+            return $this->callAction(
+                ...explode('@', $this->routes[$requestType][$uri])
+            );
         }
 
         throw new Exception('No route defined for this URI.');
+    }
+
+    protected function callAction($controller, $action)
+    {
+        die(var_dump($controller, $action));
+        
+        if(! method_exists($controller, $action)) {
+            throw new Exception(
+                "{$controller} does not respond to the {$action} action"
+            ); 
+        }
+        return (new $controller)->$action();
     }
 }
